6822835a94a69cefa1b5ecd50bd49d17f41f1a09
Edwardcm07 <ingeduarcanomontoya@gmail.com>
2022-08-22 20:59:13 -0500

    first commit

---
+++ Ejercicios_tema_8.java
@@ -0,0 +1,46 @@
+
+public class Ejercicios_tema_8 {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+
+        persona.setEdad();
+        persona.setNombre();
+        persona.setTelefono();
+
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
+
+    }
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public void setEdad() {
+        edad = 35;
+    }
+
+    public String getEdad() {
+        return "La edad es " + edad;
+    }
+
+    public void setNombre() {
+        nombre = "Alejandro";
+    }
+
+    public String getNombre() {
+        return "el nombre es  " + nombre;
+    }
+
+    public void setTelefono() {
+        telefono = 2785656;
+    }
+
+    public String getTelefono() {
+        return "el telefono es " + telefono;
+    }
+
+}
